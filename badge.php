1e2273e8043eeeaf04b8bd9f634b7f37db6fce39
Xi-Plus <huangxuanyuxiplus@gmail.com>
2016-02-14 14:58:54 +0800

    init

---
+++ badge.php
@@ -0,0 +1,4 @@
+Developed by
+<a href="https://www.facebook.com/people/Huang-Xuanyu/100005870494945" title="xiplus (黃宣喻)" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 16px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" target="_TOP">xiplus (黃宣喻)</a>
+<br />
+<a href="https://www.facebook.com/people/Huang-Xuanyu/100005870494945" title="xiplus (黃宣喻)" target="_TOP"><img class="img" src="https://badge.facebook.com/badge/100005870494945.162.30088521.png" style="border: 0px;" alt="" /></a>
